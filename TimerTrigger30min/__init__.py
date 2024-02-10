54de8868eb92477e96064ba91efb9864a06f5e27
adm_pmatejko <adm_pmatejko@esma.europa.eu>
2024-02-10 11:43:15 +0100

    working functions

---
+++ TimerTrigger30min/__init__.py
@@ -0,0 +1,54 @@
+import logging
+import json
+import azure.functions as func
+from datetime import datetime, timezone
+import requests
+from bs4 import BeautifulSoup
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+import os
+#https://pypi.org/project/opencensus-ext-azure/
+
+logger = logging.getLogger(__name__)
+azure_handler = AzureLogHandler(connection_string=os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING", 'default'))
+logger.addHandler(azure_handler)
+
+def parse_request_data(url):
+    response = requests.get(url)
+    html_content = response.text
+
+    # Parse HTML using BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Extract the number of logged-in users
+    online_users_text = soup.find(id="online").get_text()
+    online_users_number = int("".join(filter(str.isdigit, online_users_text)))
+
+    return online_users_number
+
+def main(req: func.TimerRequest) -> func.HttpResponse:
+    utc_timestamp = datetime.utcnow().replace(tzinfo=timezone.utc)
+    date_str = utc_timestamp.strftime('%Y-%m-%d')
+    weekday_str = utc_timestamp.strftime('%A') 
+
+    url = 'https://www.hattrick.org/pl/'
+    
+    try:
+        active_users = parse_request_data(url)
+    except Exception as e:
+        logging.error(f'{str(e)}')
+        active_users = 0
+
+    custom_dimensions = {
+        'active_users': str(active_users),
+        'date': date_str,
+        'check_time': utc_timestamp.strftime('%Y-%m-%dT%H:%M:%S'),
+        'weekday': weekday_str
+    }
+    logger.info('Hattrick active users new 2', extra={'custom_dimensions': custom_dimensions})
+
+
+    return func.HttpResponse(
+        f"This HTTP triggered function executed successfully. {active_users} Logged custom data for {date_str}.",
+        status_code=200
+    )
+
